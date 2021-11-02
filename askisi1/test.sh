b8c1e967c37d4adfb150c3b67a541e249df91a3c
Ioannis Tsirozidis <j.tsirozidis@gmail.com>
2021-11-02 14:15:08 +0200

    string array with urls

--- askisi1/test.sh
+++ askisi1/test.sh
@@ -1,4 +1,20 @@
-#!/bin/sh
-MY_MESSAGE="https://en.wikipedia.org/wiki/Linux"
-echo ${MY_MESSAGE#h*'.'}
+#!/bin/bash
+filename='list1.txt'
+n=1
+arrUrl=()
+
+while read line; do
+# reading each line
+
+firstCharacter=${line:0:1}
+if ! [[ "$firstCharacter" == "#" ]]; then
+    	arrUrl+=($line)
+        echo $line
+fi
+    
+# echo ${arrUrl[n-1]}
+n=$((n+1))
+
+done < $filename
+
 
