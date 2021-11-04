0ffb1ad6c949bd53379d021cb75e8ae8f54ae3fc
Ioannis Tsirozidis <j.tsirozidis@gmail.com>
2021-11-05 00:14:32 +0200

    Included stderr, INIT, FAILED, wget check

---
+++ askisi1/script1a.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+	
+filename='list1.txt'
+n=0
+arrUrl=()
+
+
+while read line; do
+# reading each line
+
+firstCharacter=${line:0:1}
+if ! [[ "$firstCharacter" == "#" ]]; then
+        
+        foo="$line"
+	result1=${foo:0:1}
+	for (( i=1; i<${#foo}; i++ )); do
+		currChar=${foo:$i:1}
+  		if ! [ "$currChar" == "." ] && ! [ "$currChar" == "/" ]; then
+  			result1=$result1$currChar
+  		fi
+	done
+	textname=${result1##h*:}".txt"
+	
+	
+	if test -f "$textname"; then
+    		#echo $textname" exists"
+    		hashcode=$(head -n 1 $textname)
+    		> $textname 
+    		wget -q $line -O $textname
+    		hashcode1="`md5sum $textname`"
+    		> $textname
+    		echo "$hashcode1" > "$textname"
+    		
+    		if [ "$hashcode" != "$hashcode1" ]; then
+    			echo $hashcode
+    			echo
+    			echo $hashcode1
+		fi
+    		
+    	else 
+    		echo $line" INIT"
+    		wget -q $line -O $textname
+		hashcode="`md5sum $textname`"
+		> $textname
+		echo "$hashcode" > "$textname"
+    		
+	fi
+	
+	
+fi
+
+done < $filename
