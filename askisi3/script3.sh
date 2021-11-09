507bf21d9bb32a5aa997125722722d62c5a73348
Ioannis Tsirozidis <j.tsirozidis@gmail.com>
2021-11-09 19:13:41 +0200

    output format pedning for completion

--- askisi3/script3.sh
+++ askisi3/script3.sh
@@ -2,6 +2,10 @@
 
 textbook_name=$1
 num=$2
+field_box=()
+all_words=()
+count_times=()
+counta=0
 
 while read line; do
 #line="I'm sure _I_ sha'n't be able!"
@@ -30,8 +34,39 @@ for i in "${stringarray[@]}"; do
 	done;
 	
 	if ! [ -z "$result1" ]; then
-		echo $result1
+		field_box+=("$result1")
+		#echo $result1
+		#if [[ " ${field_box[*]} " =~ " ${result1} " ]]; then
+    			#all_words+=("$result1")
+    			#when array contains value
+    			
+		#fi
+
+		#if [[ ! " ${field_box[*]} " =~ " ${result1} " ]]; then
+    			#field_box+=("$result1")
+    			#when array doesn't contain value
+		#fi
 	fi
 done;
 
 done <"$textbook_name"
+
+#echo Items: ${#field_box[@]}
+#echo Data: ${field_box[@]}
+
+> "appendmode.txt"
+for i in "${field_box[@]}"
+do
+: 
+
+echo -n $i" " >> "appendmode.txt"
+done
+
+tr -c '[:alnum:]' '[\n*]' < "appendmode.txt" | sort | uniq -c | sort -nr | head  -$num
+
+
+
+
+
+
+
